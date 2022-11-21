e33af088ffa5adac0af439a0c6d6f08ff4bf4da1
EricHarder <118255209+EricHarder@users.noreply.github.com>
2022-11-21 13:31:41 +0100

    Delete Hallo.py

--- Assignments/Eric_Harder/Hallo.py
+++
@@ -1,6 +0,0 @@
-#Hallo
-print('Hello what is your name?')
-#Eingabe Name
-name = input('My name is: \n')
-#Begrüßung
-print('Hi ' + name + '! Nice to meet you \n \nWelcome to the programming course')
