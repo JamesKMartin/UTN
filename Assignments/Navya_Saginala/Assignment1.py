d13b07f7b7e8fd0ec2d6d251a2f7e8942f1e77f5
Akira0810 <n.saginala@stud.uni-goettingen.de>
2022-12-12 15:45:00 +0100

    Assignment1

---
+++ Assignments/Navya_Saginala/Assignment1.py
@@ -0,0 +1,13 @@
+#1###
+x= input("Type your name:")
+print("Hi " +x+", How are you doing?")
+print("Welcome to the programming course,"+x+"!")
+
+#2###
+String= input("Type in a word:")
+print()
+reverese=''
+for i in reversangel string n9, 0, -1):
+reverse
+print (reverse)
+civic"§$%6{|]"
