fb3023d6f27df6e138b6851869c79772a793ab85
fabiopas <fabio.potrykus@web.de>
2023-01-29 17:36:09 +0100

    Delete Project directory

--- Project/210123_Flug/UTN-main/Project/seats.py
+++
@@ -1,6 +0,0 @@
-class Seat():
-
-    def __init__(self, seat_from_flight, seat_number, is_reserved):
-        self.seat_from_flight = seat_from_flight
-        self.seat_number = seat_number
-        self.is_reserved = is_reserved
